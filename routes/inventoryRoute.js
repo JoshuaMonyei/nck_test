afeb06d1bdd8c9c8c011f59785f6083d385622f5
Josh <monyeijosh01@gmail.com>
2021-11-19 22:45:30 +0100

    Update inventoryRoute.js

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -8,21 +8,21 @@ const { isLoggedIn, isAdmin, isUser } = require("../middlewares/auth");
 router.get("/", (req, res) => res.status(200).json("Welcome to Favour Gallery built with Node.js "));
 
 //  POST request to /inventorys to add a new product to inventory
-router.post("/inventorys", isLoggedIn, isAdmin, inventoryController.createInventory);
+router.post("/inventory", isLoggedIn, isAdmin, inventoryController.createInventory);
 
 // GET request to /inventorys to fetch all inventorys
-router.get("/inventorys", isLoggedIn, inventoryController.fecthInventorys);
+router.get("/inventory", isLoggedIn, inventoryController.fecthInventorys);
 
 // GET request to /inventorys to fetch a single inventory
-router.get("/inventorys/:id", isLoggedIn, inventoryController.fetchSingleInventory);
+router.get("/inventory/:id", isLoggedIn, inventoryController.fetchSingleInventory);
 
 // PUT request to update a single inventory
-router.put("/inventorys/:id", isLoggedIn, isAdmin, inventoryController.updateInventory);
+router.put("/inventory/:id", isLoggedIn, isAdmin, inventoryController.updateInventory);
 
 // DELETE request to delete a single inventory
-router.delete("/inventorys/:id", isLoggedIn, isAdmin, inventoryController.deleteSingleinventory);
+router.delete("/inventory/:id", isLoggedIn, isAdmin, inventoryController.deleteSingleinventory);
 
 // post request to add a product to cart
-router.delete("/inventorys/addToCart/:id", isLoggedIn, isUser, inventoryController.buyInventory);
+router.post("/inventorys/addToCart/:id", isLoggedIn, isUser, inventoryController.buyInventory);
 
 module.exports = router;
